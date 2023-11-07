252ac331b87c6297832633e0a3fd081e8038e24f
kevinrodriguez100 <150084289+kevinrodriguez100@users.noreply.github.com>
2023-11-06 22:03:33 -0600

    commit-1

---
+++ src/main/java/uth/tarea6/Tarea6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uth.tarea6;
+
+/**
+ *
+ * @author ksalo
+ */
+public class Tarea6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
