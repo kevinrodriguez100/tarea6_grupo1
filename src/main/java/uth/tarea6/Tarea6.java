1d6610b65a6bc3bafa2b3f2a20ddf60d28e21062
kevinrodriguez100 <150084289+kevinrodriguez100@users.noreply.github.com>
2023-11-07 18:10:26 -0600

    comit-6 (impresion de datos en el main)

--- src/main/java/uth/tarea6/Tarea6.java
+++ src/main/java/uth/tarea6/Tarea6.java
@@ -4,6 +4,8 @@
 
 package uth.tarea6;
 
+import helpers.Doctor;
+
 /**
  *Parte realizada por 202310060257Kevin-Rodriguez :
  * @author ksalo
@@ -11,6 +13,8 @@ package uth.tarea6;
 public class Tarea6 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+         Doctor doctor = new Doctor();
+        
+         doctor.impirmirInformacion();
     }
 }
