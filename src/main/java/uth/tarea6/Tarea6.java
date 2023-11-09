e69abbac5d99c9a49327584413152a4dea2679b7
kevinrodriguez100 <150084289+kevinrodriguez100@users.noreply.github.com>
2023-11-09 01:11:47 -0600

    commit-añadiendo la subclase deportista al main

--- src/main/java/uth/tarea6/Tarea6.java
+++ src/main/java/uth/tarea6/Tarea6.java
@@ -5,7 +5,7 @@
 package uth.tarea6;
 
 import helpers.Doctor;
-
+import helpers.Deportista;
 /**
  *Parte realizada por 202310060257Kevin-Rodriguez :
  * @author ksalo
@@ -14,7 +14,11 @@ public class Tarea6 {
 
     public static void main(String[] args) {
          Doctor doctor = new Doctor();
+        Deportista deportista = new Deportista();
         
          doctor.impirmirInformacion();
+         System.out.println("");
+         deportista.impirmirInformacion();
+             
     }
 }
